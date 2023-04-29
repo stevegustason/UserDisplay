67acd78976138c246cd29be25e775272eb528270
stevegustason <steve.gustason@gmail.com>
2023-04-29 18:35:56 -0500

    Initial Commit

---
+++ UserDisplay/UserDisplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserDisplayApp.swift
+//  UserDisplay
+//
+//  Created by Steven Gustason on 4/29/23.
+//
+
+import SwiftUI
+
+@main
+struct UserDisplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
